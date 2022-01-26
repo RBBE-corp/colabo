26b3e8215bde456a51c220715ef9dfcae2e09a7c
Babin Bohora <bohorababin1@gmail.com>
2022-01-26 16:08:07 +0900

    User has_many projects

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :org_name, :total_points, presence: true
+  has_many :projects
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
