eabc2cad439bc17390e4c5ab9d78ac1810ee9e21
Byron Lee <meetbyron@gmail.com>
2022-01-27 21:43:28 +0900

    contributions controller update syntax done

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,2 +1,22 @@
 class ContributionsController < ApplicationController
+  def edit
+    @contribution = Contribution.find(params[:id])
+  end
+
+  def update
+    case @contribution.update(contribution_params)
+    when :accepted
+      @contribution.status = :accepted
+      redirect_to contribution_path, notice: "Contribution accepted."
+    when :denied
+      @contribution.status = :denied
+      redirect_to contribution_path, notice: "Contribution denied."
+    end
+  end
+
+  private
+
+  def contribution_params
+    params.require(:contribution).permit(:status)
+  end
 end
