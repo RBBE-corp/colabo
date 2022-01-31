edaf38d202183b4f32d40254bf797b7a41503f71
Byron Lee <meetbyron@gmail.com>
2022-01-31 22:57:40 +0900

    can make and delete contributions to project

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,4 +1,7 @@
 class ContributionsController < ApplicationController
+  before_action :find_project, only: [ :create]
+  # after_action :verify_authorized, except: [:new, :create, :destroy]
+
   def new
     @contribution = Contribution.new
   end
@@ -6,9 +9,24 @@ class ContributionsController < ApplicationController
   def create
     @contribution = Contribution.new
     @contribution.user = current_user # user does not own this project, user is logged in
-    @contribution.project = Project.find(params[:id])
+    @contribution.project = @project
     @contribution.status = :pending
+    @contribution.start_date = DateTime.current
+    authorize @contribution # just before saving
     @contribution.save
-    redirect_to contributions_path
+    redirect_to project_path(@project)
+  end
+
+  def destroy
+    @contribution = Contribution.find(params[:id])
+    authorize @contribution
+    @contribution.destroy
+    redirect_to projects_path # don't need project_path
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:project_id])
   end
 end
