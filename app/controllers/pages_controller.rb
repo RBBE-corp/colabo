89ab50fcc31c28ad45b96b1e7ecc6a735ca0b785
Babin Bohora <bohorababin1@gmail.com>
2022-01-31 11:37:24 +0900

    components page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :components]
 
   def home
     @projects = Project.contributions_count
   end
+
+  def components; end
 end
