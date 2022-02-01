ac68ce91b88725d1846c000fe35e2ab7896037c8
Byron Lee <meetbyron@gmail.com>
2022-02-01 18:54:39 +0900

    added user_bio column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_043947) do
+ActiveRecord::Schema.define(version: 2022_02_01_094915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2022_01_29_043947) do
     t.string "user_name"
     t.integer "total_points"
     t.string "org_name"
+    t.text "user_bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
