6f4421343378dc75342ba587d82383f9dde49b9d
Babin Bohora <bohorababin1@gmail.com>
2022-02-06 21:15:32 +0900

    working with map

--- app/javascript/plugins/map.js
+++ app/javascript/plugins/map.js
@@ -3,28 +3,21 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-
-  const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 13, duration: 0 });
-};
+  navigator.geolocation.getCurrentPosition((data) => {
+    console.log(data.coords);
+  
+  // const fitMapToMarkers = (map, markers) => {
+  // const bounds = new mapboxgl.LngLatBounds();
+//   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  // map.fitBounds(bounds, { padding: 70, maxZoom: 13, duration: 0 });
+// };
 
   if (mapElement) {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
-    });
-    const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach(marker => {
-      new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .addTo(map);
-    }
-    )
-    fitMapToMarkers(map, markers)
-  };
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [data.coords.longitude, data.coords.latitude],
 };
 
 
