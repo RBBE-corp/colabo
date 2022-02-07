702ecb5c5002ea0a3d8c1926f77c8cecdd1549d9
Babin Bohora <bohorababin1@gmail.com>
2022-02-07 20:33:04 +0900

    previous production only geocode code removed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,25 +3,12 @@ class PagesController < ApplicationController
 
   def home
     @projects = Project.contributions_count
-
-    @markers = @projects.geocoded.map do |project|
+    projects = Project.all
+    @markers = projects.geocoded.map do |project|
       {
         lat: project.latitude,
         lng: project.longitude
       }
-    end
-
-    if Rails.env.production?
-      city = request.location.city
-      if city
-        @projects_near = Project.near(city, 100)
-        @markers = @projects_near.geocoded.map do |project|
-          {
-            lat: project.latitude,
-            lng: project.longitude
-          }
-        end
-      end
     end
   end
 
