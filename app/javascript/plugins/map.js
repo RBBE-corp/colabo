5a0437baca006b2add56020e7ad7cfa620cd6bf3
Babin Bohora <bohorababin1@gmail.com>
2022-02-07 20:30:22 +0900

    location permission added to main-map

--- app/javascript/plugins/map.js
+++ app/javascript/plugins/map.js
@@ -1,34 +1,53 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+const tracker = (map) => {
+  // user location tracker with permission.
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+      })
+    );
+}
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-  navigator.geolocation.getCurrentPosition((data) => {
-    console.log(data.coords);
   
-      // const fitMapToMarkers = (map, markers) => {
-      // const bounds = new mapboxgl.LngLatBounds();
-    //   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-      // map.fitBounds(bounds, { padding: 70, maxZoom: 13, duration: 0 });
-    // };
+  const fitMapToMarkers = (map, markers) => {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 13, duration: 0 });
+  };
 
-    if (mapElement) {
-      mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-      const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v10',
-        center: [data.coords.longitude, data.coords.latitude],
-        zoom: 10
-      });
-      const markers = JSON.parse(mapElement.dataset.markers);
-      markers.forEach(marker => {
-        new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(map);
-      });
-    };
-  });
-}
+  if (mapElement) {
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+
+        
+    // markers for map
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach(marker => {
+      new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(map);
+    });
+
+    if (mapElement.classList.contains("main-map")) {
+      tracker(map);
+    }
+    // calling boundary
+    fitMapToMarkers(map, markers);
+  };
+};
 
 
 export { initMapbox };
