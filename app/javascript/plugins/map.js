f2dc20d612ffa35c601515a121d8bfb1b5ab31fe
Babin Bohora <bohorababin1@gmail.com>
2022-02-07 19:17:01 +0900

    map with current address done

--- app/javascript/plugins/map.js
+++ app/javascript/plugins/map.js
@@ -6,19 +6,29 @@ const initMapbox = () => {
   navigator.geolocation.getCurrentPosition((data) => {
     console.log(data.coords);
   
-  // const fitMapToMarkers = (map, markers) => {
-  // const bounds = new mapboxgl.LngLatBounds();
-//   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  // map.fitBounds(bounds, { padding: 70, maxZoom: 13, duration: 0 });
-// };
+      // const fitMapToMarkers = (map, markers) => {
+      // const bounds = new mapboxgl.LngLatBounds();
+    //   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+      // map.fitBounds(bounds, { padding: 70, maxZoom: 13, duration: 0 });
+    // };
 
-  if (mapElement) {
-    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10',
-      center: [data.coords.longitude, data.coords.latitude],
-};
+    if (mapElement) {
+      mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v10',
+        center: [data.coords.longitude, data.coords.latitude],
+        zoom: 10
+      });
+      const markers = JSON.parse(mapElement.dataset.markers);
+      markers.forEach(marker => {
+        new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+      });
+    };
+  });
+}
 
 
 export { initMapbox };
